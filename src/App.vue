eb0f1af9b5b64473e94cea47785cd13efa3bde3e
reed665 <reed665@hotmail.com>
2020-04-08 13:09:17 +0300

    init layout module with AppLayout component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,17 @@
 <template>
-  <div id="app">
-    <header>
-      <div class="logo">tmrrw</div>
-
-      <nav>
-        <router-link class="nav-item" #default="{ isExactActive, navigate }" to="/">
-          <TmButton :text="!isExactActive" @click="navigate">Today</TmButton>
-        </router-link>
-
-        <router-link class="nav-item" #default="{ isExactActive, navigate }" to="/tmrrw">
-          <TmButton :text="!isExactActive" @click="navigate">Tomorrow</TmButton>
-        </router-link>
-      </nav>
-    </header>
-
-    <main>
-      <router-view />
-    </main>
-  </div>
+  <AppLayout>
+    <router-view />
+  </AppLayout>
 </template>
 
 <script>
-import { TmButton } from '@/modules/ui-kit';
+import { AppLayout } from '@/modules/layout';
 
 export default {
   components: {
-    TmButton,
+    AppLayout
   }
-  
+
 }
 </script>
 
@@ -43,39 +27,5 @@ body {
   font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
-
-#app {
-  header {
-    border: 2px solid red;
-    padding: 18px 48px;
-    display: flex;
-    align-items: center;
-
-    .logo {
-      position: relative;
-      bottom: 2px;
-      font-family: 'Open Sans', sans-serif;
-      font-size: 1.8rem;
-      font-weight: bold;
-      font-style: italic;
-    }
-
-    nav {
-      display: flex;
-
-      .nav-item {
-        margin-left: 25px;
-
-        &:first-child {
-          margin-left: 40px;
-        }
-      }
-    }
-  }
-
-  main {
-    border: 2px solid blue;
-  }
 }
 </style>
---
+++ src/modules/layout/components/AppLayout.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="logo">tmrrw</div>
+
+      <nav>
+        <router-link class="nav-item" #default="{ isExactActive, navigate }" to="/">
+          <TmButton :text="!isExactActive" @click="navigate">Today</TmButton>
+        </router-link>
+
+        <router-link class="nav-item" #default="{ isExactActive, navigate }" to="/tmrrw">
+          <TmButton :text="!isExactActive" @click="navigate">Tomorrow</TmButton>
+        </router-link>
+      </nav>
+    </header>
+
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script>
+import { TmButton } from '@/modules/ui-kit';
+
+export default {
+  components: {
+    TmButton,
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  border: 2px solid red;
+  padding: 18px 48px;
+  display: flex;
+  align-items: center;
+
+  .logo {
+    position: relative;
+    bottom: 2px;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 1.8rem;
+    font-weight: bold;
+    font-style: italic;
+  }
+
+  nav {
+    display: flex;
+
+    .nav-item {
+      margin-left: 25px;
+
+      &:first-child {
+        margin-left: 40px;
+      }
+    }
+  }
+}
+
+main {
+  border: 2px solid blue;
+}
+</style>
