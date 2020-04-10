30d969dcd93c6751b74b2bbc7094be462e61e9c5
reed665 <reed665@hotmail.com>
2020-04-10 10:36:42 +0300

    TmButtonInputForm: add some magic

--- src/modules/ui-kit/components/TmButtonInputForm.vue
+++ src/modules/ui-kit/components/TmButtonInputForm.vue
@@ -1,13 +1,23 @@
 <template>
   <form @submit.prevent="trySubmit">
-    <input type="text" v-model="inputValue">
+    <button type="button"
+      v-show="isButtonMode"
+      @click="switchToInputMode"
+    >
+      {{ actionText }}
+    </button>
 
-    <button>{{ actionText }}</button>
+    <input type="text"
+      ref="inputRef"
+      v-show="isInputMode"
+      v-model="inputValue"
+      @blur="switchToButtonMode"
+    >
   </form>
 </template>
 
 <script>
-import { ref } from '@vue/composition-api';
+import { ref, computed } from '@vue/composition-api';
 
 export default {
   props: {
@@ -17,21 +27,38 @@ export default {
     }
   },
 
-  setup(props, { emit }) {
+  setup(props, { emit, refs, root }) {
     const inputValue = ref('');
+    const isInputMode = ref(false);
+    const isButtonMode = computed(() => !isInputMode.value);
+
+    const switchToInputMode = () => {
+      isInputMode.value = true;
+
+      root.$nextTick()
+        .then(() => {
+          refs.inputRef.focus();
+        })
+    }
+
+    const switchToButtonMode = () => {
+      isInputMode.value = false;
+    }
 
     const trySubmit = () => {
-      if (!inputValue.value) {
-        return;
+      if (inputValue.value) {
+        emit('submit', inputValue.value);
+        inputValue.value = '';
       }
-      
-      emit('submit', inputValue.value);
-      
-      inputValue.value = '';
+      switchToButtonMode();
     }
 
     return {
       inputValue,
+      isInputMode,
+      isButtonMode,
+      switchToInputMode,
+      switchToButtonMode,
       trySubmit,
     }
   }
