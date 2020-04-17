9db24b3a9425ca526ab4ffd320a0869012159849
reed665 <abuiko@outlook.com>
2020-04-17 15:08:34 +0300

    vue add vue-next

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue';
 
 import './plugins/composition-api'
 import './plugins/touch-events'
@@ -6,9 +6,4 @@ import './plugins/touch-events'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).use(router).mount('#app')
